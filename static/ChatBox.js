0a050b42e9a20b2d83a2b0a35213252029ec7ba6
01060c <xxs239239@gmail.com>
2025-05-30 13:24:09 +0530

    Update ChatBox.js

--- static/ChatBox.js
+++ static/ChatBox.js
@@ -3,12 +3,13 @@ window.ChatBox = class ChatBox {
     this.element = document.getElementById('chatbox');
   }
 
-  addMessage(nick, value) {
+  addMessage(nick, color, value) {
     let messageBox = document.createElement('div');
     messageBox.className = 'message-box';
     let nickDisplay = document.createElement('span');
     nickDisplay.className = 'nick-display';
     nickDisplay.innerText = `${nick}: `;
+    nickDisplay.style.color = "hsl("+color+", 100%, 50%)"
     let messageDisplay = document.createElement('span');
     messageDisplay.className = 'message-content';
     messageDisplay.innerText = value;
